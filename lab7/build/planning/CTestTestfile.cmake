a0a966722d0d0d208f38b610401d12c6ffe5e13d
Class Account <ee106a-ahh@c105-1.local>
2021-11-16 11:25:24 -0800

    adding lab 7

---
+++ lab7/build/planning/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106a/fl21/class/ee106a-ahh/ros_workspaces/lab7/src/planning
+# Build directory: /home/cc/ee106a/fl21/class/ee106a-ahh/ros_workspaces/lab7/build/planning
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
